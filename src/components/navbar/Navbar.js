b7b0010305fbbb4eb82e67040e56feaa9559f510
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2022-10-20 00:57:40 +0330

    compelete navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,7 +7,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -32,83 +31,79 @@ const Navbar = () => {
     };
 
     const pages = [{
-        title: "بازی میراژ",
-        link: "/mirageGame"
-    }, {
         title: "صفحه اصلی",
         link: "/"
+    }, {
+        title: "بازی میراژ",
+        link: "/mirageGame"
     }, {
         title: "وبلاگ",
         link: "/blog"
     }];
-    const settings = [<Instagram />, <WhatsApp />, <Telegram />];
+    const settings = [
+        <Instagram className={classes.socialIcon} />,
+        <WhatsApp className={classes.socialIcon} />,
+        <Telegram className={classes.socialIcon} />
+    ];
 
     return (
-        <AppBar className={classes.appBar} color='inherit'>
-            <Container maxWidth="lg" >
-                <Toolbar disableGutters>
+        <AppBar className={classes.appBarContainer}>
+            <Toolbar disableGutters className={classes.toolBarBox}>
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: { xs: 'block', md: 'none' },
+                        }}>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}>
-
-                            {pages.map((page) => (
-                                <MenuItem key={page.title} onClick={handleCloseNavMenu} >
-                                    <Link to={page.link} textAlign="center">{page.title}</Link>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page.title}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 1, pl: 6, color: 'black', display: 'block' }}
-                            >
-                                <Link to={page.link} textAlign="center">{page.title}</Link>
-                            </Button>
+                        {pages.map((page, index) => (
+                            <MenuItem key={index} onClick={handleCloseNavMenu} >
+                                <Link to={page.link}>{page.title}</Link>
+                            </MenuItem>
                         ))}
-                    </Box>
-                    <LazyLoadImage src={logo} className={classes.logo} />
-                    <Box sx={{ flexGrow: 1, ml: 32, display: { xs: 'none', md: 'flex' } }}>
-                        {settings.map((setting) => (
-                            <Button
-                                key={setting}
-                                sx={{ my: 1, pl: 7, color: 'black', display: 'block' }}
-                            >
-                                {setting}
-                            </Button>
-                        ))}
-                    </Box>
+                    </Menu>
+                </Box>
+                <Box className={classes.menuIconBox} sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    {settings.map((setting, index) => (
+                        <Button className={classes.menuIconBtn} key={index}>
+                            {setting}
+                        </Button>
+                    ))}
+                </Box>
+                <div className={classes.logoBox}>
+                    <Link to='/'>
+                        <LazyLoadImage src={logo} className={classes.logo} />
+                    </Link>
+                </div>
+                <Box className={classes.menuItemBox} sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    {pages.map((page, index) => (
+                        <Link key={index} className={classes.menuItemLink} to={page.link}>{page.title}</Link>
+                    ))}
+                </Box>
 
-                </Toolbar>
-            </Container>
+            </Toolbar>
         </AppBar>
     );
 };
